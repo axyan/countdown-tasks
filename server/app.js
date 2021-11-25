224a3b51146bb5701838eaf0f26ef928fca161d1
Andrew Yan <git@andrewyan.dev>
2021-11-25 05:26:55 -0800

    Implement app error handler for 404

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
+const createError = require('http-errors');
 const logger = require('morgan');
+const path = require('path');
 
 const db = require('./config/database');
 const indexRouter = require('./routes/index');
@@ -14,5 +15,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', indexRouter);
+
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({ error: err.message });
+});
 
 module.exports = app;
