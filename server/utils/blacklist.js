365b571befc338c822fe6a4fd5dfdbe9da1d7795
Andrew Yan <git@andrewyan.dev>
2021-12-23 21:42:15 -0800

    Refactor promise to async/await syntax

--- server/utils/blacklist.js
+++ server/utils/blacklist.js
@@ -1,26 +1,22 @@
 const redisClient = require('../config/cache');
 
-exports.add = (key, expiration) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      // Set 0 as all keys' value to minimize memory consumption
-      // Set EXAT to expire entry at token expiration
-      await redisClient.set(key, 0, { EXAT: expiration });
-      resolve({ key, expiration });
-    } catch (err) {
-      reject(err);
-    }
-  });
+exports.add = async (key, expiration) => {
+  try {
+    // Set 0 as all keys' value to minimize memory consumption
+    // Set EXAT to expire entry at token expiration
+    await redisClient.set(key, 0, { EXAT: expiration });
+    return { key, expiration };
+  } catch (err) {
+    throw err;
+  }
 };
 
-exports.exists = (key) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      // Data returned if key exists in blacklist
-      const data = await redisClient.get(key);
-      data !== null ? resolve(true) : resolve(false);
-    } catch (err) {
-      reject(err);
-    }
-  });
+exports.exists = async (key) => {
+  try {
+    // Data returned if key exists in blacklist
+    const data = await redisClient.get(key);
+    return data !== null ? true : false;
+  } catch (err) {
+    throw err;
+  }
 };
